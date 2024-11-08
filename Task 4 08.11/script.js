27e9854d637b76f1fd6b15fd39ebfaa322aa6c39
ShIreen66 <066shiree@gmail.com>
2024-11-08 20:01:02 +0530

    styles

---
+++ Task 4 08.11/script.js
@@ -0,0 +1,9 @@
+const body = document.querySelector("body");
+console.log(body); // jab kisi bhi key ko dabate hai to key up and down ho jate hai
+body.addEventListener("keydown", (e) => {
+    const element = document.querySelector('${e.key}');
+    element.style.backgroundColor = "black"
+    element.style.
+    element.childNodes[1].play();
+    console.log(e.key);
+});
